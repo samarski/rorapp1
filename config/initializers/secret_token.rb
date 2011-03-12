3d1a54bc5eadcf8f5e55b30dba69640301cad241
Aleksandar Balasev-Samarski <samarski@gmail.com>
2011-03-12 20:24:56 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rorapp1::Application.config.secret_token = '4672147dc75845444148e27b8246ed5738b8116e17f78e0325c8eb69c6ba5bb74d4d8add53b9687e2ee4a1a7906d1568cc4f82dde4b66406f12d0e084e3c3f61'
